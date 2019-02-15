af5736d483100be4eb9bd3ca0ce8b7f55294cdbd
ctq123 <chengtianqing123@sina.cn>
2019-02-15 14:50:30 +0800

    添加standard代码规范化

--- public/index.js
+++ public/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppRouter from '../src/AppRouter';
-import * as serviceWorker from '../src/serviceWorker';
+import ReactDOM from 'react-dom'
+import './index.css'
+import AppRouter from '../src/AppRouter'
+import * as serviceWorker from '../src/serviceWorker'
 
 window.RELEASE = RELEASE
-ReactDOM.render(AppRouter, document.getElementById('root'));
+ReactDOM.render(AppRouter, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
--- src/About.js
+++ src/About.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Form, Switch, Slider, Button } from 'antd';
+import React from 'react'
+import { Form, Switch, Slider, Button } from 'antd'
 import { Link } from 'react-router'
-import './About.css';
+import './About.css'
 
 class About extends React.PureComponent {
-  render() {
+  render () {
     return (
       <Form>
         <Form.Item
-          label="开关"
+          label='开关'
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 8 }}
         >
           <Switch defaultChecked />
         </Form.Item>
         <Form.Item
-          label="滑动输入条"
+          label='滑动输入条'
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 8 }}
         >
           <Slider defaultValue={70} />
         </Form.Item>
         <Form.Item
-          label="logo"
+          label='logo'
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 8 }}
         >
@@ -32,7 +32,7 @@ class About extends React.PureComponent {
           style={{ marginTop: 48 }}
           wrapperCol={{ span: 8, offset: 8 }}
         >
-          <Button type="primary" htmlType="submit">
+          <Button type='primary' htmlType='submit'>
             确定
           </Button>
           <Button style={{ marginLeft: 8 }}>
@@ -40,7 +40,7 @@ class About extends React.PureComponent {
           </Button>
         </Form.Item>
         <Form.Item
-          label="转跳"
+          label='转跳'
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 8 }}
         >
@@ -53,4 +53,4 @@ class About extends React.PureComponent {
   }
 }
 
-export default About;
+export default About
